eece15a0d5a9a9d4f5318eb9d7b545bf976ef376
Elias de Francisco <eliasdefrancisco@gmail.com>
2019-08-20 11:27:54 +0200

    Listening slot change event

--- modal.js
+++ modal.js
@@ -69,6 +69,10 @@ class Modal extends HTMLElement {
                 </section>
             </div>
         `
+        const slots = this.shadowRoot.querySelectorAll('slot')
+        slots[1].addEventListener('slotchange', event => {
+            console.dir(slots[1].assignedNodes())
+        })
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
