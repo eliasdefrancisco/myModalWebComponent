be193b1a06bb4425c2fd3a5661fa907f45dbce4c
Elias de Francisco <eliasdefrancisco@gmail.com>
2019-08-21 12:10:18 +0200

    Some css, animations and minor stuff

--- modal.js
+++ modal.js
@@ -18,7 +18,7 @@ class Modal extends HTMLElement {
                 }
                 #modal {
                     position: fixed;
-                    top: 15vh;
+                    top: 10vh;
                     left: 25%;
                     width: 50%;
                     z-index: 100;
@@ -30,20 +30,26 @@ class Modal extends HTMLElement {
                     justify-content: space-between;
                     opacity: 0;
                     pointer-events: none;
+                    transition: all 0.05s ease-out;
                 }
                 :host([opened]) #backdrop,
                 :host([opened]) #modal{
                     opacity: 1;
                     pointer-events: all;
                 }
+                :host([opened]) #modal {
+                    top: 15vh;
+                }
                 #main {
                     padding: 1rem;
                 }
                 header {
                     padding: 1rem;
+                    border-bottom: 1px solid #ccc;
                 }
                 ::slotted(h1) {
                     font-size: 1.25rem;
+                    margin: 0;
                 }
                 #actions {
                     border-top: 1px solid #ccc;
@@ -73,8 +79,10 @@ class Modal extends HTMLElement {
         slots[1].addEventListener('slotchange', event => {
             console.dir(slots[1].assignedNodes())
         })
+        const backdrop = this.shadowRoot.querySelector('#backdrop')
         const cancelButton = this.shadowRoot.querySelector('#cancel-btn')
         const confirmButton = this.shadowRoot.querySelector('#confirm-btn')
+        backdrop.addEventListener('click', this._cancel.bind(this))
         cancelButton.addEventListener('click', this._cancel.bind(this))
         confirmButton.addEventListener('click', this._confirm.bind(this))
         // cancelButton.addEventListener('cancel', () => {
